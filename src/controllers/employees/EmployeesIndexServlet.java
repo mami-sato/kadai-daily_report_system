140db4db1bf4ec4a1181f3e4b63e65eac9a04c26
Mame <mmmmaaaa0000@outlook.jp>
2021-02-18 22:06:26 +0900

    Add Employee edit

--- src/controllers/employees/EmployeesIndexServlet.java
+++ src/controllers/employees/EmployeesIndexServlet.java
@@ -37,21 +37,21 @@ public class EmployeesIndexServlet extends HttpServlet {
         int page = 1;
         try{
             page = Integer.parseInt(request.getParameter("page"));
-        }catch(NumberFormatException e){}
-        List<Employee>employees = em.createNamedQuery("getAllEmployees",Employee.class)
-                                    .setFirstResult(15 * (page - 1))
-                                    .setMaxResults(15)
-                                    .getResultList();
+        } catch(NumberFormatException e) { }
+        List<Employee> employees = em.createNamedQuery("getAllEmployees", Employee.class)
+                                     .setFirstResult(15 * (page - 1))
+                                     .setMaxResults(15)
+                                     .getResultList();
 
-        long employees_count = (long)em.createNamedQuery("getEmployeeCount",Long.class)
-                                        .getSingleResult();
+        long employees_count = (long)em.createNamedQuery("getEmployeesCount", Long.class)
+                                       .getSingleResult();
 
         em.close();
 
-        request.setAttribute("employee", employees);
+        request.setAttribute("employees", employees);
         request.setAttribute("employees_count", employees_count);
         request.setAttribute("page", page);
-        if(request.getSession().getAttribute("flush")!=null){
+        if(request.getSession().getAttribute("flush") != null) {
             request.setAttribute("flush", request.getSession().getAttribute("flush"));
             request.getSession().removeAttribute("flush");
         }
@@ -59,5 +59,4 @@ public class EmployeesIndexServlet extends HttpServlet {
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/index.jsp");
         rd.forward(request, response);
     }
-
 }
--- src/models/Employee.java
+++ src/models/Employee.java
@@ -13,10 +13,22 @@ import javax.persistence.Table;
 
 @Table(name = "employees")
 @NamedQueries({
-        @NamedQuery(name = "getAllEmployees", query = "SELECT e FROM Employee AS e ORDER BY e.id DESC"),
-        @NamedQuery(name = "getEmployeeCount", query = "SELECT COUNT(e) FROM Employee AS e"),
-        @NamedQuery(name="checkRegisteredCode", query="SELECT COUNT(e) FROM Employee AS e WHERE e.code = :code"),
-        @NamedQuery(name = "checkLoginCodeAndPassWord", query = "SELECT e FROM Employee AS e WHERE e.delete_flag = 0 AND e.code = :code AND e.password = :pass")
+    @NamedQuery(
+        name = "getAllEmployees",
+        query = "SELECT e FROM Employee AS e ORDER BY e.id DESC"
+    ),
+    @NamedQuery(
+        name = "getEmployeesCount",
+        query = "SELECT COUNT(e) FROM Employee AS e"
+    ),
+    @NamedQuery(
+        name = "checkRegisteredCode",
+        query = "SELECT COUNT(e) FROM Employee AS e WHERE e.code = :code"
+    ),
+    @NamedQuery(
+        name = "checkLoginCodeAndPassword",
+        query = "SELECT e FROM Employee AS e WHERE e.delete_flag = 0 AND e.code = :code AND e.password = :pass"
+    )
 })
 @Entity
 public class Employee {
@@ -109,5 +121,4 @@ public class Employee {
     public void setDelete_flag(Integer delete_flag) {
         this.delete_flag = delete_flag;
     }
-
 }
